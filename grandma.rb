295b1636a10cf9dc025a04614e7e74fba10d0cf0
Lukie Kang <lukiek+f@gmail.com>
2019-05-20 12:42:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,13 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma(phrase)
 
+if phase == "I LOVE YOU GRANDMA!"
+elseif phrase == " "#notuppoercase(phrase)
+  "HUH?! Speak Up, Sonny!"
+else 
+  "NO, NOT SINCE 1938!"
+
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).

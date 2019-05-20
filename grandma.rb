0820139ab30ea5f3d5e2110c3f57c33fb54ba234
Lukie Kang <lukiek+f@gmail.com>
2019-05-20 12:48:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,8 +4,8 @@ def speak_to_grandma(phrase)
   puts shouted_phrase
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == " "#notuppoercase(phrase)
-    "HUH?! Speak Up, Sonny!"
+  elsif phrase != shouted_phrase
+    "HUH?! SPEAK UP SONNY!"
   else
     "NO, NOT SINCE 1938!"
   end
